17d032df099b75842f5d2a708709b35c93d8a52c
Yin <yinnnn17@gmail.com>
2021-01-02 00:21:51 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+
+export default function App() {
+	return (
+		<View style={styles.screen}>
+			<Header title="Guess A Number" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+	},
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# guess-a-number
+# guess-a-number
